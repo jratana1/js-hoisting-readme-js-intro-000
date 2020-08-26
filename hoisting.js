890307bc54d98add46f2c75028b003f942403117
John Ratanaprasatporn <jratana1@gmail.com>
2020-08-26 03:15:16 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -23,11 +23,12 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-
+  
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
+    
   }
   sayMy();
 }
